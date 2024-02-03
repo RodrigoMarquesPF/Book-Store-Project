e1ea2288cd9590e34a6a3f930200f41719a921f0
RodrigoMarquesPF <rodrigomarquespf@gmail.com>
2024-02-02 21:58:24 -0300

    shadcn adicionado em js

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "../../components/ui/button";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>Home</div>
+      <Button>Teste</Button>
+    </>
+  );
+};
 
-export default Home
+export default Home;
